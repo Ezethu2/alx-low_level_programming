007cece0402a7fde247063c7d0932379f521899b
Ezethu2 <hebeezethu2@gmai.com>
2023-07-17 12:28:34 +0200

    5-printf.c

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,1 +1,10 @@
-:hh
+#include<stdio.h>
+/**
+  * main - this is the main function
+  * Return: Always 0 (success)
+  */
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
