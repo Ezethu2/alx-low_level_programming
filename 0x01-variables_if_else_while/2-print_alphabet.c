b6d6cde95b8cf36d5770955c631fa439837790dd
Ezethu2 <hebeezethu2@gmai.com>
2023-07-18 23:44:04 +0200

    1-last_digit.c

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -1,20 +1,2 @@
-#include <stdio.h >
-#include <ctype.h>
 
-/**
-  * main - prints alpha in lower case
-  *
-  * Return: Always 0
-  */
 
-int main(void)
-{
-int lowercase = 'a';
-while (lower <= 'z')
-{
-putchar(lowercase);
-lowercase += 1;
-}
-putchar('\n');
-return (0);
-{
