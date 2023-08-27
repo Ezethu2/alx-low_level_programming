2698e6f38a22e13438e0bacfbc37c859aac66740
Ezethu2 <hebeezethu2@gmai.com>
2023-08-27 22:30:44 +0200

    Write a function that adds a new node at the beginning of a list_t list.

--- 0x12-singly_linked_lists/2-add_node.c
+++ 0x12-singly_linked_lists/2-add_node.c
@@ -1,48 +1,30 @@
-#include "lists.h"
 #include <stdlib.h>
-int len(const char *str);
+#include <string.h>
+#include "lists.h"
+
 /**
-  * add_node - add a node tp the begining of the list
-  * @head: pointer to the head of the list
-  * @str: string to be insert to the first positiopn
-  * Return: pointer to the first node (newly assigned one)
-  */
+ * add_node - adds a new node at the beginning of a linked list
+ * @head: double pointer to the list_t list
+ * @str: new string to add in the node
+ *
+ * Return: the address of the new element, or NULL if it fails
+ */
 list_t *add_node(list_t **head, const char *str)
 {
-	list_t *new_node;
+	 list_t *new;
+	 unsigned int len = 0;
 
-	if (head == NULL)
+	while (str[len])
+	len++;
+
+	new = malloc(sizeof(list_t));
+	if (!new)
 		return (NULL);
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
-		return (NULL);
 
-	if (*head == NULL)
+	new->str = strdup(str);
+	new->len = len;
+	new->next = (*head);
+	(*head) = new;
 
-		new_node->next =( NULL);
-	else
-			new_node->next = *head;
-		new_node->str = strdup(str);
-		new_node->len = len(str);
-		*head = new_node;
 	return (*head);
-}
-/**
-  * len - get the length of str (string)
-  * @str: the string to get its length
-  * Return: length of the string
-  */
-int len(const char *str)
-{
-	int i;
-
-	if (str == NULL)
-		return (0);
-
-	i = 0;
-	while (str[i] != '\0')
-	{
-		i++;
-	}
-	return (i);
 }
